e58a15f587518a8fede99b71138fc4924e805e0f
uqyanzie <uqyanzie@gmail.com>
2024-03-18 06:19:31 +0700

    Fix dockerfile

--- docker-start.sh
+++ docker-start.sh
@@ -10,4 +10,4 @@ docker run -d -p 8080:8080 -p 9901:9901 --network=host envoy:1.0
 
 docker build -t go-grpc-server:1.0 -f server/Dockerfile server
 
-docker run -d  -p 9090:9090 --env-file .env go-grpc-server:1.0 
+docker run -d  -p 9090:9090 --env-file server/.env go-grpc-server:1.0 
